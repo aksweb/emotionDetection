e038ca5280790f87a569c08611f442b6938cd6df
Abhishekh Kumar Singh <98414194+aksweb@users.noreply.github.com>
2022-08-25 21:22:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+# 1.Loading the libraries
+# 2.Using CascadeClassifier to classify the images
+# 3.Loading the model through keras load_module library and save it to var classifier
+# 4.Creating array of emotions as emotion_labels.
+# 5.Capturing video.
+# 6.prediction =  classifier.predict()
+#   label = emotion_labels[prediction.argmax()]    //for giving labels to the emotions.
+
+from keras.models import load_model
+from time import sleep
+from keras.utils import img_to_array
+from keras.preprocessing import image
+
+
+# from keras.preprocessing.image import img_to_array
+import cv2
+import numpy as np
+
+face_classifier = cv2.CascadeClassifier(r'C:\Users\Abhishekh\Documents\smart music player\Emotion_Detection_CNN-main\haarcascade_frontalface_default.xml')
+classifier =load_model(r'C:\Users\Abhishekh\Documents\smart music player\Emotion_Detection_CNN-main\model.h5')
+
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+
+cap = cv2.VideoCapture(0)
+
+
+
+while True:
+    _, frame = cap.read()
+    labels = []
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi,axis=0)
+
+            prediction = classifier.predict(roi)[0]
+            label=emotion_labels[prediction.argmax()]
+            label_position = (x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+        else:
+            cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cv2.imshow('Emotion Detector',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
